0ec500e1a0b3cd4e1053d71fd9840b23b1f2f5fe
sauves <leonardsauve@gmail.com>
2023-07-28 21:22:14 -0400

    latest changes

--- survival.R
+++ survival.R
@@ -28,3 +28,42 @@ risk[preds > median(preds)] = "high"
 brca_fit = left_join(data.frame(case_id = test_ids, risk = risk ), brca_surv)
 km_fit = survfit(Surv(survt, surve) ~ risk ,data = brca_fit )
 autoplot(km_fit)
+
+
+# data
+#lgn = read.csv("Data/LEUCEGENE/lgn_pronostic_GE_CDS_TPM.csv")
+lgn = read.csv("Data/SIGNATURES/LSC17_lgn_pronostic_expressions.csv")
+lgn_clin = read.csv("Data/LEUCEGENE/lgn_pronostic_CF")
+# process
+lgn_clin$sampleID
+names(lgn)[1] = "sampleID"
+names(lgn)
+lgn$sampleID
+
+lgn_table = left_join(lgn, lgn_clin)
+lgn_table %>% select(Overall_Survival_Status, Overall_Survival_Time_days)
+test_ids = sample(lgn_table$sampleID, nrow(lgn_table) * 0.25)
+lgn_test = lgn_table %>% filter(sampleID %in% test_ids)
+lgn_train = lgn_table %>% filter(!(sampleID %in% test_ids))
+# train
+mdl_fit = coxph(Surv(Overall_Survival_Time_days, Overall_Survival_Status) ~  ENSG00000174059 +
+ENSG00000104341  + ENSG00000128805 + ENSG00000095932 + ENSG00000277988 + 
+ENSG00000138722 + ENSG00000088305 + ENSG00000120833 + ENSG00000105810 + ENSG00000238049 + 
+ENSG00000088882 + ENSG00000130584 + ENSG00000113657 + ENSG00000205978 + ENSG00000261633 + 
+ENSG00000196139 + ENSG00000134531, data = lgn_train)
+summary(mdl_fit)
+# test
+preds = -as.numeric(predict(mdl_fit, type = "risk", newdata = lgn_test))
+#preds = -as.numeric(preds)
+metrics = concordance(Surv(Overall_Survival_Time_days, Overall_Survival_Status) ~ preds, 
+data = lgn_test)
+metrics
+hist(preds)
+risk = rep("low", nrow(lgn_test))
+risk[preds > median(preds)] = "high"
+lgn_fit = left_join(data.frame(sampleID= test_ids, risk = risk ), lgn_clin)
+km_fit = survfit(Surv(Overall_Survival_Time_days, Overall_Survival_Status) ~ risk ,
+data = lgn_fit )
+autoplot(km_fit) 
+# plot
+
